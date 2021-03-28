3317d237ba7f1e0dd929d434d372497a9e2b5a3f
IrhamFadzillah <irham8381@gmail.com>
2021-03-28 04:22:20 +0000

    pylint: auto fixes
    Signed-off-by: IrhamFadzillah <irham8381gmail.com>

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -6,12 +6,13 @@
 """ Userbot module containing commands related to the \
     Information Superhighway (yes, Internet). """
 
+import time
 from datetime import datetime
 
 from speedtest import Speedtest
-from userbot import CMD_HELP, StartTime, ALIVE_NAME
+
+from userbot import ALIVE_NAME, CMD_HELP, StartTime
 from userbot.events import register
-import time
 
 
 async def get_readable_time(seconds: int) -> str:
@@ -22,9 +23,7 @@ async def get_readable_time(seconds: int) -> str:
 
     while count < 4:
         count += 1
-        remainder, result = divmod(
-            seconds, 60) if count < 3 else divmod(
-            seconds, 24)
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
         if seconds == 0 and remainder == 0:
             break
         time_list.append(int(result))
@@ -54,11 +53,13 @@ async def pingme(pong):
     await pong.edit("__**⚡PING⚡**__")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
-    await pong.edit(f"**⚡ᴘɪɴɢ⚡**\n"
-                    f"⚡ **ᴘɪɴɢ:** "
-                    f"`%sms` \n"
-                    f"⚡ **ᴏɴʟɪɴᴇ:** "
-                    f"`{uptime}` \n" % (duration))
+    await pong.edit(
+        f"**⚡ᴘɪɴɢ⚡**\n"
+        f"⚡ **ᴘɪɴɢ:** "
+        f"`%sms` \n"
+        f"⚡ **ᴏɴʟɪɴᴇ:** "
+        f"`{uptime}` \n" % (duration)
+    )
 
 
 @register(outgoing=True, pattern=r"^\.awping$")
@@ -107,12 +108,14 @@ async def pingme(pong):
     await pong.edit("**100% ██████████**")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
-    await pong.edit(f"- 𝐋 𝐎 𝐑 𝐃 -\n"
-                    f"**• ꜱɪɴʏᴀʟ  :** "
-                    f"`%sms` \n"
-                    f"**• ᴏɴʟɪɴᴇ  :** "
-                    f"`{uptime}` \n"
-                    f"**• ᴏᴡɴᴇʀ  :** `{ALIVE_NAME}`" % (duration))
+    await pong.edit(
+        f"- 𝐋 𝐎 𝐑 𝐃 -\n"
+        f"**• ꜱɪɴʏᴀʟ  :** "
+        f"`%sms` \n"
+        f"**• ᴏɴʟɪɴᴇ  :** "
+        f"`{uptime}` \n"
+        f"**• ᴏᴡɴᴇʀ  :** `{ALIVE_NAME}`" % (duration)
+    )
 
 
 @register(outgoing=True, pattern="^.speed$")
@@ -127,28 +130,30 @@ async def speedtst(spd):
     test.results.share()
     result = test.results.dict()
 
-    await spd.edit("**Hasil Tes:\n**"
-                   "❃ **Dimulai Pada:** "
-                   f"`{result['timestamp']}` \n"
-                   f" **━━━━━━━━━━━━━━━━━**\n\n"
-                   "❃ **Download:** "
-                   f"`{speed_convert(result['download'])}` \n"
-                   "❃ **Upload:** "
-                   f"`{speed_convert(result['upload'])}` \n"
-                   "❃ **Ping:** "
-                   f"`{result['ping']}` \n"
-                   "❃ **ISP:** "
-                   f"`{result['client']['isp']}` \n"
-                   "❃ **BOT:** `ezbw`")
+    await spd.edit(
+        "**Hasil Tes:\n**"
+        "❃ **Dimulai Pada:** "
+        f"`{result['timestamp']}` \n"
+        f" **━━━━━━━━━━━━━━━━━**\n\n"
+        "❃ **Download:** "
+        f"`{speed_convert(result['download'])}` \n"
+        "❃ **Upload:** "
+        f"`{speed_convert(result['upload'])}` \n"
+        "❃ **Ping:** "
+        f"`{result['ping']}` \n"
+        "❃ **ISP:** "
+        f"`{result['client']['isp']}` \n"
+        "❃ **BOT:** `ezbw`"
+    )
 
 
 def speed_convert(size):
     """
     Hi human, you can't read bytes?
     """
-    power = 2**10
+    power = 2 ** 10
     zero = 0
-    units = {0: '', 1: 'Kb/s', 2: 'Mb/s', 3: 'Gb/s', 4: 'Tb/s'}
+    units = {0: "", 1: "Kb/s", 2: "Mb/s", 3: "Gb/s", 4: "Tb/s"}
     while size > power:
         size /= power
         zero += 1
@@ -169,15 +174,21 @@ async def pingme(pong):
     duration = (end - start).microseconds / 9000
     await pong.edit("✘ **Ping!**\n`%sms`" % (duration))
 
+
 CMD_HELP.update(
-    {"ping": "`.ping` ; `.lping` ; `.awping`\
+    {
+        "ping": "`.ping` ; `.lping` ; `.awping`\
     \nPenjelasan: Untuk menunjukkan ping bot.\
     \n\n`.speed`\
     \nPenjelasan: Untuk menunjukkan kecepatan.\
     \n\n`.pong`\
     \nPenjelasan: sama kaya perintah ping."
-     })
+    }
+)
 CMD_HELP.update(
-    {"sinyal": "**Modules:** `Sinyal`\
+    {
+        "sinyal": "**Modules:** `Sinyal`\
     \n\n**• Perintah :** `.sinyal`\
-    \n  ➥ **Penjelasan :** __Untuk melihat sinyal bot__"})
+    \n  ➥ **Penjelasan :** __Untuk melihat sinyal bot__"
+    }
+)
